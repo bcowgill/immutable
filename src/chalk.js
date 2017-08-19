1dbb755654150b0732a6edb55375f9c8a8204468
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-08-19 17:01:48 +0100

    chalk.js logging functions similar to console with different color for each type.

--- src/chalk.js
+++ src/chalk.js
@@ -1,10 +1,61 @@
-const chalk = require('chalk')
+// chalk.js - coloured logging for error, warn, log, info, debug
 
-module.exports =
+const chalk = require('chalk')
+const konsole = console
+
+function str (obj)
 {
-	error: chalk.bold.red,
-	warn:  chalk.keyword('orange'),
-	log:   chalk.white,
-	info:  chalk.cyan,
-	debug: chalk.green,
+	return typeof obj === 'object' ? JSON.stringify(obj) : obj
 }
+
+const write =
+{
+	chalk: chalk,
+
+	color: {
+		error: chalk.red,
+		warn:  chalk.keyword('orange'),
+		log:   chalk.white,
+		info:  chalk.cyan,
+		debug: chalk.green,
+
+		alt: {
+			alt: void 0,
+
+			error: chalk.bold.red,
+			warn:  chalk.bold.keyword('orange'),
+			log:   chalk.bold.white,
+			info:  chalk.bold.cyan,
+			debug: chalk.bold.green,
+		},
+	},
+
+	getLogger: function (type, logger)
+	{
+		type = type || 'log'
+		logger = logger || konsole
+		const colors = [write.color[type], write.color.alt[type]]
+		const log = logger[type] || logger.log
+
+		return function ()
+		{
+			const argz = Array.prototype.slice.call(arguments)
+			for (var idx = 0; idx < argz.length; ++idx)
+			{
+				argz[idx] = colors[idx % 2](str(argz[idx]))
+			}
+			log.call(logger, argz.join(' '))
+		}
+	},
+}
+write.color.alt.alt = write.color
+
+Object.keys(write.color).forEach(function (type) {
+	if (type !== 'alt')
+	{
+		write[type] = write.getLogger(type)
+	}
+})
+
+module.exports = write
+
--- src/chalk.spec.js
+++ src/chalk.spec.js
@@ -20,24 +20,74 @@ describe('chalk', function () { with (this) { addSkip(this)
 		return mapped
 	}
 
-	it('error text is colored ' + mychalk.error('red'), function () { with (this) {
-		console.error(mychalk.error('chalk error text'))
-		console.warn(mychalk.warn('chalk warn text'))
-		console.log(mychalk.log('chalk log text'))
-		console.info(mychalk.info('chalk info text'))
-		console.info(mychalk.debug('chalk debug text'))
-		const chalked = mychalk.error('error')
+	it('colours the console', function () { with (this) {
+		mychalk.error('\tchalk error text')
+		mychalk.warn('\tchalk warn text')
+		mychalk.log('\tchalk log text')
+		mychalk.info('\tchalk info text')
+		mychalk.debug('\tchalk debug text')
+
+		mychalk.error('this is the message', { 'prop': '34' }, [1,2,3], 'more text', console.log)
+		mychalk.info('this is the message', { 'prop': '34' }, [1,2,3], 'more text', console.log)
+		console.log('this is the message', { 'prop': '34' }, [1,2,3], 'more text', console.log)
+
+		assert(true)
+	}})
+
+	it('converts objects to JSON strings', function () { with (this) {
+		const logger = { }
+		expect(logger, 'error')
+			.given(match(/this is the message/))
+			.given(match(/\{"prop":"34"\}/))
+			.given(match(/\[1,2,3\]/))
+			.given(match(/more text/))
+			.given(match(/function \(\) \{ \[native code\] \}/))
+		const log = mychalk.getLogger('error', logger)
+		log('this is the message', { 'prop': '34' }, [1,2,3], 'more text', console.log)
+	}})
+
+	it('alternate color for each argument', function () { with (this) {
+		let got = ''
+		const logger = { error: function (string) { got = string }}
+		const log = mychalk.getLogger('error', logger)
+		log('A', 'B', 'C', 'D')
+
 		assertEqual([
+			27, '[', '3', '1', 'm',
+			'A',
+			27, '[', '3', '9', 'm',
+			' ',
 			27, '[', '1', 'm',
+			27, '[', '3', '1', 'm',
+			'B',
+			27, '[', '3', '9', 'm',
+			27, '[', '2', '2', 'm',
+			' ',
+			27, '[', '3', '1', 'm',
+			'C',
+			27, '[', '3', '9', 'm',
+			' ',
+			27, '[', '1', 'm',
+			27, '[', '3', '1', 'm',
+			'D',
+			27, '[', '3', '9', 'm',
+			27, '[', '2', '2', 'm',
+			], mapCharCodes(got))
+	}})
+
+	it('error text is colored ' + mychalk.color.error('red'), function () { with (this) {
+
+
+		const chalked = mychalk.color.error('error')
+		assertEqual([
 			27, '[', '3', '1', 'm',
 			'e', 'r', 'r', 'o', 'r',
 			27, '[', '3', '9', 'm',
-			27, '[', '2', '2', 'm'
 			], mapCharCodes(chalked))
 	}})
 
-	it('warning text is colored ' + mychalk.warn('orange'), function () { with (this) {
-		const chalked = mychalk.warn('error')
+	it('warning text is colored ' + mychalk.color.warn('orange'), function () { with (this) {
+		const chalked = mychalk.color.warn('error')
 		assertEqual([
 			27, '[', '9', '3', 'm',
 			'e', 'r', 'r', 'o', 'r',
@@ -45,8 +95,8 @@ describe('chalk', function () { with (this) { addSkip(this)
 			], mapCharCodes(chalked))
 	}})
 
-	it('log text is colored ' + mychalk.log('white'), function () { with (this) {
-		const chalked = mychalk.log('error')
+	it('log text is colored ' + mychalk.color.log('white'), function () { with (this) {
+		const chalked = mychalk.color.log('error')
 		assertEqual([
 			27, '[', '3', '7', 'm',
 			'e', 'r', 'r', 'o', 'r',
@@ -54,8 +104,8 @@ describe('chalk', function () { with (this) { addSkip(this)
 			], mapCharCodes(chalked))
 	}})
 
-	it('info text is colored ' + mychalk.info('cyan'), function () { with (this) {
-		const chalked = mychalk.info('error')
+	it('info text is colored ' + mychalk.color.info('cyan'), function () { with (this) {
+		const chalked = mychalk.color.info('error')
 		assertEqual([
 			27, '[', '3', '6', 'm',
 			'e', 'r', 'r', 'o', 'r',
@@ -63,8 +113,8 @@ describe('chalk', function () { with (this) { addSkip(this)
 			], mapCharCodes(chalked))
 	}})
 
-	it('debug text is colored ' + mychalk.debug('green'), function () { with (this) {
-		const chalked = mychalk.debug('error')
+	it('debug text is colored ' + mychalk.color.debug('green'), function () { with (this) {
+		const chalked = mychalk.color.debug('error')
 		assertEqual([
 			27, '[', '3', '2', 'm',
 			'e', 'r', 'r', 'o', 'r',
